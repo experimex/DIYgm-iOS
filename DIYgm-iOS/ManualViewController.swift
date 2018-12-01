575b9e7e6cdca60b7583629aa140f0051896e40b
Max <limax2012@gmail.com>
2018-12-01 18:13:38 -0500

    Implement show/hide popup tools button

--- DIYgm-iOS/ManualViewController.swift
+++ DIYgm-iOS/ManualViewController.swift
@@ -16,15 +16,14 @@ class ManualViewController: UIViewController {
     var navView: UIView?
     var mapView: GMSMapView?
     var toolsView: UIView?
-    let countField = UITextField(frame: CGRect(x: 10, y: 10, width: 150, height: 30))
+    var popupToolsView: UIView?
+    var countField: UITextField? = UITextField(frame: CGRect(x: 20, y: 10, width: 150, height: 30))
     var markerCount: Int = 0
     var markers: Array<GMSMarker> = Array()
+    var popupToolsHidden = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Open keyboard automatically
-        countField.becomeFirstResponder()
         
         self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.976, green: 0.976, blue: 0.976, alpha: 1.0)
         self.navigationController?.navigationBar.isTranslucent = false
@@ -38,26 +37,42 @@ class ManualViewController: UIViewController {
         self.view.addSubview(mapView!)
         
         // Tools view
-        let toolsRect = CGRect(x: 0, y: (mapView?.frame.size.height)!, width: self.view.frame.size.width, height: 100)
+        let toolsRect = CGRect(x: 0, y: (mapView?.frame.size.height)!, width: self.view.frame.size.width, height: keyboardHeight! + 50)
         toolsView = UIView(frame: toolsRect)
         toolsView?.backgroundColor = UIColor.white
         self.view.addSubview(toolsView!)
         
-        // Text field to enter count rate
-        countField.placeholder = "Count Rate"
-        countField.textAlignment = .center
-        countField.font = countField.font?.withSize(28)
-        countField.borderStyle = UITextField.BorderStyle.roundedRect
-        countField.keyboardType = UIKeyboardType.decimalPad
-        toolsView?.addSubview(countField)
+        // Tools view: Text field to enter count rate
+        countField!.becomeFirstResponder() // Open keyboard automatically
+        countField!.placeholder = "Count Rate"
+        countField!.textAlignment = .center
+        countField!.font = countField!.font?.withSize(28)
+        countField!.borderStyle = UITextField.BorderStyle.roundedRect
+        countField!.keyboardType = UIKeyboardType.decimalPad
+        toolsView?.addSubview(countField!)
         
-        // Set count rate button
+        // Tools view: Set count rate button
         let setButton = UIButton(type: UIButton.ButtonType.system)
-        setButton.frame = CGRect(x: countField.frame.size.width + 20, y: 10, width: 60, height: 30)
+        setButton.frame = CGRect(x: countField!.frame.size.width + 25, y: 10, width: 60, height: 30)
         setButton.setTitle("Set", for: .normal)
         setButton.titleLabel?.font = setButton.titleLabel?.font.withSize(24)
         setButton.addTarget(self, action: #selector(setCountRate(_:)), for: .touchUpInside)
         toolsView?.addSubview(setButton)
+        
+        // Tools view: Show popup tools button
+        let showPopupButton = UIButton(type: UIButton.ButtonType.system)
+        showPopupButton.frame = CGRect(x: self.view.frame.size.width - 80, y: 10, width: 60, height: 30)
+        showPopupButton.setTitle("Tools", for: .normal)
+        showPopupButton.titleLabel?.font = setButton.titleLabel?.font.withSize(24)
+        showPopupButton.addTarget(self, action: #selector(showPopupTools(_:)), for: .touchUpInside)
+        toolsView?.addSubview(showPopupButton)
+        
+        // Popup tools view
+        let popupToolsRect = CGRect(x: self.view.frame.size.width - 200, y: (mapView?.frame.size.height)!, width: 200, height: 200)
+        popupToolsView = UIView(frame: popupToolsRect)
+        popupToolsView!.backgroundColor = UIColor.blue
+        self.view.addSubview(popupToolsView!)
+        self.view.bringSubviewToFront(toolsView!)
         
         /*
         // Keyboard toolbar with set and done button
@@ -69,13 +84,13 @@ class ManualViewController: UIViewController {
         let flexSpaceRight = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         toolbar.setItems([flexSpaceLeft, setButton, undoButton, removeAllButton, flexSpaceRight], animated: false)
         toolbar.sizeToFit()
-        self.countField.inputAccessoryView = toolbar
+        self.countField!.inputAccessoryView = toolbar
         */
     }
     
     // From Set button on keyboard
     @objc func setCountRate(_ sender: UIButton) {
-        if (countField.text == "") {
+        if (countField!.text == "") {
             print("No count rate entered")
         }
         else if (mapView?.myLocation) == nil {
@@ -87,7 +102,7 @@ class ManualViewController: UIViewController {
             let currentLocation = CLLocationCoordinate2D(latitude: (mapView?.myLocation?.coordinate.latitude)!, longitude: (mapView?.myLocation?.coordinate.longitude)!)
             marker.position = currentLocation
             marker.title = "Marker\(markerCount)"
-            marker.snippet = countField.text
+            marker.snippet = countField!.text
             marker.map = mapView
             markers.append(marker)
             
@@ -95,8 +110,24 @@ class ManualViewController: UIViewController {
             mapView!.animate(toLocation: currentLocation)
             
             print ("Recorded \(String(describing: marker.snippet)) at \(String(describing: marker.title))")
-            countField.text = ""
+            countField!.text = ""
         }
+    }
+    
+    @objc func showPopupTools(_ sender: UIButton) {
+        self.view.bringSubviewToFront(toolsView!)
+        
+        if (popupToolsHidden) { // Move it up 200 pixels
+            UIView.animate(withDuration: 0.2, delay: 0, options: [], animations: {
+                self.popupToolsView!.frame = CGRect(x: self.view.frame.size.width - 200, y: (self.mapView?.frame.size.height)! - 200, width: 200, height: 200)
+            })
+        }
+        else { // Move it down 200 pixels
+            UIView.animate(withDuration: 0.2, delay: 0, options: [], animations: {
+                self.popupToolsView!.frame = CGRect(x: self.view.frame.size.width - 200, y: (self.mapView?.frame.size.height)!, width: 200, height: 200)
+            })
+        }
+        self.popupToolsHidden = !self.popupToolsHidden
     }
     
     @objc func undoMarker(_ sender: UIButton) {
