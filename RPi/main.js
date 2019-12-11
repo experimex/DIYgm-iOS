8c80a122258eb1a547f1b13a6f4ecf2072891804
experimex <limax2012@gmail.com>
2019-12-11 18:05:53 -0500

    Update UUID

--- RPi/main.js
+++ RPi/main.js
@@ -10,7 +10,7 @@ var name = "";
 bleno.on('stateChange', function(state) {
   if (state === 'poweredOn') {
     name = fs.readFileSync("name.txt").toString();
-    bleno.startAdvertising(name, ['e3754285-8072-458b-a45b-94a0dab368ef']);
+    bleno.startAdvertising(name, ['e3754285-8072-458b-a45b-94a0dab36800']);
   } else {
     bleno.stopAdvertising();
   }
@@ -21,7 +21,7 @@ bleno.on('advertisingStart', function(error) {
     console.log("iOS server - started advertising as " + name);
     bleno.setServices([
       new BlenoPrimaryService({
-        uuid: 'e3754285-8072-458b-a45b-94a0dab368ef',
+        uuid: 'e3754285-8072-458b-a45b-94a0dab36800',
         characteristics: [
           new EchoCharacteristic()
         ]
